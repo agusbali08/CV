98024d8ec16ab2e190e405d70fdf6d1cc2a6038e
agusbali08 <115845573+agusbali08@users.noreply.github.com>
2023-02-28 02:43:07 -0300

    CV_FINAL

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 var myCV = new mydetails("Kurt Riley","Web designer","kurt.riley@example.com",
-"(667) 905-5001","1743 Preston Rd","2/4/1988","OBJECTIVE","",
-"EDUCATION","GRADUATION","","INTERMEDIATE","","MATRICULATION","",
+"(667) 905-5001","1743 Preston Rd","2/4/1988","OBJECTIVE","Highly motivated university graduate seeking a full-time position with an agency where I can develop my knowledge in the field to help achieve organizational goals.",
+"EDUCATION","GRADUATION","2006-20011<br>Informatic in Universidad de Georgetown","INTERMEDIATE","UDC","MATRICULATION","School Without Walls High School","LANGUAJES","English,Spanish,French",
 "EXPERIENCE","WORK","I work 1 year in a hospital as informatic tecnic",
 "SKILLS","HTML5","CSS3","JAVASCRIPT","BOOTSTRAP","C","JAVA","ADOBE CC","AUTOCAD","MS-OFFICE 365","CONTACT")
 
-function mydetails(name,det,gmail,phone,adrs,birthday,headobj,objective,headedu,matric,school,inter,college,grad,uni,headexp,work,hospital,
+function mydetails(name,det,gmail,phone,adrs,birthday,headobj,objective,headedu,matric,school,inter,college,grad,uni,lang,idioms,headexp,work,hospital,
 headskills,html,css,jscript,boot,c,java,adobe,autocad,office,contact)
 {
 this.name=name;
@@ -22,6 +22,8 @@ this.inter=inter;
 this.college=college;
 this.grad=grad;
 this.uni=uni;
+this.lang=lang;
+this.idioms=idioms;
 this.headexp=headexp;
 this.work=work;
 this.hospital=hospital;
@@ -54,6 +56,8 @@ document.getElementById("levels").childNodes[5].innerHTML=inter;
 document.getElementById("levels").childNodes[7].innerHTML=college;
 document.getElementById("levels").childNodes[9].innerHTML=grad;
 document.getElementById("levels").childNodes[11].innerHTML=uni;
+document.getElementById("levels").childNodes[13].innerHTML=lang;
+document.getElementById("levels").childNodes[15].innerHTML=idioms;
 
 document.getElementById("experience").childNodes[2].innerHTML=headexp;
 document.getElementById("levels2").childNodes[1].innerHTML=work;
@@ -80,21 +84,29 @@ function blueclr(){
     document.getElementById("e1").style.backgroundColor= "#0800FF";
     document.getElementById("e2").style.backgroundColor= "#0800FF";
     document.getElementById("e3").style.backgroundColor= "#0800FF";
+    document.getElementById("e4").style.backgroundColor= "#0800FF";
+    document.getElementById("e5").style.backgroundColor= "#0800FF";
     document.getElementById("skills").style.color= "#0800FF";
     document.getElementById("percent").style.backgroundColor= "#0800FF";
     document.getElementById("CONTACT").style.color= "#0800FF";
     document.getElementById("map").style.backgroundColor= "#0800FF";
-    document.getElementById("cpyrt").style.backgroundColor= "#0800FF";
 
     document.getElementById("objective").style.color= "#13385f";
     document.getElementById("objctv").style.color= "#13385f";
     document.getElementById("education").style.color= "#13385f";
+    document.getElementById("experience").style.color= "#13385f";
+    document.getElementById("skills").style.color= "#13385f";
+    document.getElementById("CONTACT").style.color= "#13385f";
     document.getElementById("p1").style.color= "#13385f";
     document.getElementById("p1").style.borderLeftColor= "rgb(0, 179, 104)";
     document.getElementById("p2").style.color= "#13385f";
     document.getElementById("p2").style.borderLeftColor= "rgb(0, 179, 104)";
     document.getElementById("p3").style.color= "#13385f";
     document.getElementById("p3").style.borderLeftColor= "rgb(0, 179, 104)";
+    document.getElementById("p4").style.color= "#13385f";
+    document.getElementById("p4").style.borderLeftColor= "rgb(0, 179, 104)";
+    document.getElementById("p5").style.color= "#13385f";
+    document.getElementById("p5").style.borderLeftColor= "rgb(0, 179, 104)";
     document.getElementById("s1").style.backgroundColor= "#13385f";
     document.getElementById("s2").style.backgroundColor= "#13385f";
     document.getElementById("s3").style.backgroundColor= "#13385f";
@@ -104,5 +116,4 @@ function blueclr(){
     document.getElementById("s7").style.backgroundColor= "#13385f";
     document.getElementById("s8").style.backgroundColor= "#13385f";
     document.getElementById("s9").style.backgroundColor= "#13385f";
-
 }
